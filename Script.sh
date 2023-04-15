587118cb6c42953d0eed02ed15faf1ff04a416fe
ErinTUDelft <93988345+ErinTUDelft@users.noreply.github.com>
2023-04-15 19:03:31 +0200

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -24,5 +24,38 @@ module load py-torchvision
 module load py-tensorboardX
 module load py-timm
 module load py-imageio
+module load py-scikit-image
+module load py-scipy
+module load py-tifffile
+module load py-PyWavelets
+module load py-requests
+module load py-PyYAML
+module load py-setuptools
+module load py-tqdm
+module load py-typing_extensions
+module load py-urllib3
+
+module load py-certifi                                                
+module load py-charset-normalizer                                   
+module load py-contourpy                                              
+module load py-cycler                                                 
+module load py-filelock                                               
+module load py-fonttools                                              
+module load py-huggingface-hub                                        
+module load py-idna                                                  
+
+module load py-importlib-resources                                      
+module load py-install                                                 
+module load py-kiwisolver                                            
+module load py-lazy-loader                                            
+linear-warmup-cosine-annealing-warm-restarts-weight-decay 1.0
+module load py-matplotlib                                              
+module load py-networkx                                               
+
+module load py-packaging                                               
+module load py-Pillow                                                  
+module load py-pyparsing                                               
+module load py-python-dateutil                                          
+
 
 srun python train.py --data_path "/scratch/elucassen/kitti_data" --model_name Erin_train_Saturday_1 --batch_size 16
