16d8a8b6e310d6d66bc4e363cdf1936016edd955
ErinTUDelft <93988345+ErinTUDelft@users.noreply.github.com>
2023-04-16 14:29:55 +0200

    Update Script-new.sh

--- Script-new.sh
+++ Script-new.sh
@@ -8,13 +8,6 @@
 #SBATCH --partition=gpu
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --account=education-ae-msc-ae
-
-
 module load 2022r2
-
-module load openmpi
-module load python
-module load py-numpy
-module load py-mpi4py
-                                     
-srun python train.py --data_path "/scratch/elucassen/kitti_data" --model_name Erin_train_Saturday_1 --batch_size 6 >Erin.$$.log 2>Erin.$$.err
+module load openmpi                       
+srun /scratch/elucassen/Lite-Mono-Reproduction/LocalPython/bin/python3 train.py --data_path "/scratch/elucassen/kitti_data" --model_name Erin_train_Saturday_1 --batch_size 6 >Erin.$$.log 2>Erin.$$.err
